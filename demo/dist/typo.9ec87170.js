6f4085f3e8ec165b7d2cc998a3be493ee570e2d1
Luca Gesmundo <lucagesmundo@yahoo.it>
2018-11-29 23:31:14 +0100

    .

--- demo/dist/typo.9ec87170.js
+++ demo/dist/typo.9ec87170.js
@@ -203,7 +203,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37315" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39613" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
